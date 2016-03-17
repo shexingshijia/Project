6d1a0242ffb720a388306f364fa59ff249ecea65
shege <shexingshijia@gmail.com>
2016-03-17 17:20:51 -0400

    project

--- src/hotelDBMS/DBConnection.java
+++ src/hotelDBMS/DBConnection.java
@@ -36,19 +36,34 @@ public class DBConnection {
 		
 	}	
 	
-	public void addCutsomer(String fName, String lName, Integer IDnumber, String emailAddress, String country){
+	public void addCustomer(Integer refNumber, String fName, String lName, String IDnumber, String emailAddress, String country){
 		try {
 			// create new Statement
 			Statement myStmt = conn.createStatement();
 			// the entity with need to be added
-			String entity = fName + "," + lName + "," + IDnumber + "," + emailAddress + "," + country ;
+			String entity = refNumber + ",'" + fName + "','" + lName + "','" + IDnumber + "','" + emailAddress + "','" + country +"'";
 			// the query for inserting
-			String query = "insert into Customer"
-					+ " (fName, lName, IDnumber, emailAddress, country)"
-					+ "values (" + entity + ")"; 
-			
+			String query = "INSERT INTO customers"
+					+ "(refNumber, fName, lName, IDnumber, emailAddress, country)"
+					+ "VALUES(" + entity + ")"; 
+			// 
 			myStmt.executeUpdate(query);
-			System.out.println("Insert Complete");
+			System.out.println("New Customer Insert Completed");
+			
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	public void updateCustomer(String Category, Object data, String condition){
+		try {
+			// create new Statement
+			Statement myStmt = conn.createStatement();
+			String query = "UPDATE customers SET"
+					+ Category + "=" + data +"WHERE" + condition;
+			// 
+			myStmt.executeUpdate(query);
+			System.out.println("Customer info updated Completed");
 			
 		} catch (SQLException e) {
 			// TODO Auto-generated catch block
@@ -56,11 +71,24 @@ public class DBConnection {
 		}
 	}
 
-	public void addRooms(){
-		
+	public void addRooms(Integer roomNumber, String roomType, Integer capacity, double price, Integer customerRef){
+		try {
+			// create new Statement
+			Statement myStmt = conn.createStatement();
+			// the entity with need to be added
+			String entity = roomNumber + ",'" + roomType + "'," + capacity + "," + price + "," + customerRef;
+			// the query for inserting
+			String query = "INSERT INTO room"
+					+ "(roomNumber, roomType, capacity, Price, customerRef)"
+					+ "VALUES(" + entity + ")"; 
+			myStmt.executeUpdate(query);
+			System.out.println("New Room Insert Completed");
+			
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
-	
-	
 	
 	
 	public static void main(String[] args) {
@@ -75,19 +103,10 @@ public class DBConnection {
 //			  }
 		DBConnection myDB = new DBConnection("jdbc:mysql://localhost:3306/firstSchema", "root", "260225towncenter");
 		myDB.executeQuery("SELECT * FROM firstschema.customers;");
-		//myDB.addCutsomer("a", "b", 1234, "123@123", "CAN");
-		
-		
-		
-		
-	
+		//myDB.addCustomer(9,"FIRST","LAST","1234","hellogmail.com","ca");
+		//myDB.addRooms(234,"window",3, 899.99, 99);
+		myDB.updateCustomer("fName","UTSC","fName = U");
+		myDB.executeQuery("SELECT * FROM firstschema.customers;");
 	}
-
-	
-	
-	
-	
-	
-	
 }
 
